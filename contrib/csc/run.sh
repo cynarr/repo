2c1e80ab8700fd64f61726cc65dd032bb8721775
Frankie Robertson <frankie@robertson.name>
2021-02-05 07:27:20 +0200

    Update CSC cluster config

--- contrib/csc/run.sh
+++ contrib/csc/run.sh
@@ -1,7 +1,25 @@
-SIF_PATH=/projappl/project_2003933/sifs/image_latest.sif \
+#!/bin/bash
+
+PRE_SCRIPT=$(cat <<'END_PRE_SCRIPT'
+# From CSC singularity_wrapper
+CSC_SING_FLAGS="-B /users:/users -B /projappl:/projappl -B /scratch:/scratch -B /appl/data:/appl/data"
+if [ -n "$TMPDIR" ]; then
+    CSC_SING_FLAGS="$SING_FLAGS -B $TMPDIR:$TMPDIR"
+fi
+if [ -n "$LOCAL_SCRATCH" ]; then
+    CSC_SING_FLAGS="$SING_FLAGS -B $LOCAL_SCRATCH:$LOCAL_SCRATCH"
+fi
+SING_EXTRA_ARGS="$CSC_SING_FLAGS $SING_EXTRA_ARGS"
+END_PRE_SCRIPT
+)
+
+PRE_SCRIPT="$PRE_SCRIPT" \
+SIF_PATH=/projappl/project_2003933/sifs/pipe_latest.sif \
 SNAKEFILE=/opt/coviddash/ingress/workflow/Snakefile \
 CLUSC_CONF=`pwd`/clusc.json \
 SING_EXTRA_ARGS="--bind `pwd`/clusc.json" \
 NUM_JOBS=64 \
 NO_CLEANUP=1 \
-/projappl/project_2003933/singslurm2/run.sh
+TRACE=1 \
+OVERRIDE_TMPDIR=/scratch/project_2003933/tmp/ \
+/projappl/project_2003933/singslurm2/run.sh all
