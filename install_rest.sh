72c9525af2795662cc33d1d7939b0754e95bc8e1
Frankie Robertson <frankie@robertson.name>
2021-02-08 07:24:24 +0200

    Update news-please

--- install_rest.sh
+++ install_rest.sh
@@ -3,4 +3,4 @@
 set -x trace -euo pipefail
 
 # Doesn't work with Poetry
-pip install --force-reinstall git+https://github.com/mood-mapping-muppets/news-please.git@a0f2490aac7b76dd1bd90f41d53ea2a636588956
+pip install --force-reinstall git+https://github.com/mood-mapping-muppets/news-please.git@5ed9b804bf9434815e1783f17942aa8ae585907d
