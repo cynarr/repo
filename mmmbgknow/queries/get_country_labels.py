5be336f39406df1628ee1a25e6845e0b05456776
Frankie Robertson <frankie@robertson.name>
2021-02-10 10:58:29 +0200

    Update country labels to include countries within countries (England, Catalonia)

--- mmmbgknow/queries/get_country_labels.py
+++ mmmbgknow/queries/get_country_labels.py
@@ -3,35 +3,54 @@ from ..european import EURO_COUNTRIES, EURO_LANGUAGES
 
 wikidata = Wikidata()
 
-QUERY = """
-SELECT ?langcode ?cc2 ?lab
-WHERE
-{
-  # has 2-letter country code
-  ?country wdt:P297 ?cc2 .
-  # cc2 in my set of values
-  VALUES ?cc2 { %s } .
-  # langcode in my set of values
-  VALUES ?langcode { %s } .
-  {
-      # country has label
-      ?country rdfs:label ?lab
-  } UNION {
-      # or altLabel
-      ?country skos:altLabel ?lab
-  } .
-  # label in language
-  FILTER (lang(?lab) = ?langcode)
-}
-""" % (
-    " ".join(('"{}"'.format(cc2) for cc2 in EURO_COUNTRIES)),
-    " ".join(('"{}"'.format(langcode) for langcode in EURO_LANGUAGES)),
-)
+def make_query(ccs, langcodes):
+    return """
+        SELECT ?langcode ?cc2 ?lab
+        WHERE
+        {
+          # has 2-letter country code
+          ?country wdt:P297 ?cc2 .
+          # is or has adminRegion (reifying neccesary to include England for some reason)
+          ?country (p:P150/ps:P150)* ?adminRegion .
+          # is a country (reifying neccesary to include England for some reason)
+          ?adminRegion p:P31/ps:P31/wdt:P279* wd:Q6256 .
+          # not a historical country
+          FILTER NOT EXISTS {?adminRegion wdt:P31/wdt:P279* wd:Q3024240} .
+          # cc2 in my set of values
+          VALUES ?cc2 { %s } .
+          # langcode in my set of values
+          VALUES ?langcode { %s } .
+          {
+              # adminRegion has label
+              ?adminRegion rdfs:label ?lab .
+          } UNION {
+              # or altLabel
+              ?adminRegion skos:altLabel ?lab
+          } .
+          FILTER (lang(?lab) = ?langcode)
+        }
+        GROUP BY ?langcode ?cc2 ?lab
+    """ % (
+        " ".join(('"{}"'.format(cc2) for cc2 in ccs)),
+        " ".join(('"{}"'.format(langcode) for langcode in langcodes)),
+    )
+
+
+results = []
+for country in EURO_COUNTRIES:
+    # It's a slow query so we just go country at a time to avoid timeouts
+    results.extend(
+        wikidata.query_tpl(
+            make_query((country,), EURO_LANGUAGES),
+            "langcode",
+            "cc2",
+            "lab"
+        )
+    )
+results.sort()
 
 
 print("langcode,cc2,lab")
 
-for langcode, cc2, lab in sorted(
-    wikidata.query_tpl(QUERY, "langcode", "cc2", "lab")
-):
+for langcode, cc2, lab in results:
     print(f"{langcode},{cc2},{lab}")
