5449f546a1ca3da49a9cf7734cadbf18ea6ecfb0
Alok Debnath <alok.debnath@research.iiit.ac.in>
2021-02-17 04:41:48 +0530

    BERT+TF_IDF done, need to extract labels

--- topic_modeler/data.py
+++ topic_modeler/data.py
@@ -8,6 +8,9 @@ from bertopic import BERTopic
 from sentence_transformers import SentenceTransformer
 import umap.umap_ as umap
 import hdbscan
+import pandas as pd
+
+TOKENIZERS_PARALLELISM = False
 
 def decompress_zstandard_to_folder(input_file):
     input_file = pathlib.Path(input_file)
@@ -20,6 +23,8 @@ def decompress_zstandard_to_folder(input_file):
 
 def jsonl_reader(input_file):
     textio = dict() # the text for each language
+    links = list()
+    print("Splitting articles by language:")
     with open(input_file, 'rb') as f:
         jlist = list(f)
         for jstr in tqdm(jlist):
@@ -28,20 +33,30 @@ def jsonl_reader(input_file):
             if res['maintext'] is None: continue
             if l not in textio: textio[l] = io.StringIO()
             textio[l].write(res['maintext'])
+            links.append(res['canon_url'])
     textblob = {}
     for l in textio:
        textblob[l] = textio[l].getvalue()
        textio[l].close()
-    return textblob
+    return textblob, links
 
-def bert_topic(lang, langcode, textblob):
+def bert_topic(lang, textblob, links):
     model = SentenceTransformer('distiluse-base-multilingual-cased-v2')
-    embeddings = model.encode(textblob[langcode].split('\n')[:2000], show_progress_bar=True)
+    print("Generating article embeddings for " + lang)
+    embeddings = model.encode(textblob[lang].split('\n')[:20000], show_progress_bar=True)
+
     umap_embeddings = umap.UMAP(n_neighbors=15, n_components=5, metric='cosine').fit_transform(embeddings)
     cluster = hdbscan.HDBSCAN(min_cluster_size=15, metric='euclidean', cluster_selection_method='eom').fit(umap_embeddings)
-    print(cluster.labels_)
+    print(len(cluster.labels_))
+    
+    docs_df = pd.DataFrame(textblob[lang].split('\n')[:20000], columns=['doc'])
+    docs_df['topic'] = cluster.labels_
+    docs_df['doc_id'] = range(len(docs_df))
+    docs_df['canon_url'] = links[:20000]
+    docs_per_topic = docs_df.groupby(['topic'], as_index = False).agg({'doc': ' '.join})
+    print(docs_per_topic)
 
 if __name__ == '__main__':
     # decompress_zstandard_to_folder('./covidstatebroadcaster.fixed.jsonl.*')
-    tb = jsonl_reader('./covidstatebroadcaster.fixed.jsonl')
-    bert_topic('english', 'en', tb)
+    tb, links = jsonl_reader('./covidstatebroadcaster.fixed.jsonl')
+    bert_topic('en', tb, links)
