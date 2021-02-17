f978ac140650eba5b66df063590f6ce084e8e7b9
Frankie Robertson <frankie@robertson.name>
2021-02-17 17:06:16 +0200

    Fix mixed tabs/spaces in database_schema

--- database/database_schema.sql
+++ database/database_schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE documents (
     document_id INTEGER PRIMARY KEY,
-	canon_url TEXT UNIQUE,
-	date_publish INTEGER,	-- Unix format dates
-	title TEXT,
-	country TEXT,
-	language TEXT
+    canon_url TEXT UNIQUE,
+    date_publish INTEGER,	-- Unix format dates
+    title TEXT,
+    country TEXT,
+    language TEXT
 );
 
 CREATE TABLE moral_sentiment_scores (
-	canon_url TEXT NOT NULL,
+    canon_url TEXT NOT NULL,
     sentiment_type TEXT NOT NULL,
-	score REAL NOT NULL,
-	PRIMARY KEY (canon_url, sentiment_type)
+    score REAL NOT NULL,
+    PRIMARY KEY (canon_url, sentiment_type)
 );
 
 CREATE TABLE mbert_sentiment (
