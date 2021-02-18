e74580291394c3fddbe666dfa9d70a85ce888a02
Alok Debnath <alok.debnath@research.iiit.ac.in>
2021-02-18 22:29:59 +0530

    Added the link/topic combination json file

--- topic_modeler/data.py
+++ topic_modeler/data.py
@@ -48,10 +48,12 @@ def bert_topic(lang, textblob, links):
     print("Generating article embeddings for " + lang + ":")
     embeddings = model.encode(textblob[lang].split('\t\t')[:-1], 
             show_progress_bar=True)
-
-    umap_embeddings = umap.UMAP(n_neighbors=15, 
-            n_components=5, 
-            metric='cosine').fit_transform(embeddings)
+    try:
+        umap_embeddings = umap.UMAP(n_neighbors=15, 
+                n_components=5, 
+                metric='cosine').fit_transform(embeddings)
+    except:
+        return
     cluster = hdbscan.HDBSCAN(min_cluster_size=15, 
             metric='euclidean', 
             cluster_selection_method='eom').fit(umap_embeddings)
@@ -66,8 +68,11 @@ def bert_topic(lang, textblob, links):
     # print(docs_df)
     return embeddings, cluster, docs_df, docs_per_topic
 
-def c_tf_idf(docs, m, stop_words, ngram_range = (1, 1)):
-    count = CountVectorizer(ngram_range=ngram_range, stop_words=stop_words).fit(docs)
+def c_tf_idf(docs, m, stop_words=None, ngram_range = (1, 1)):
+    try:
+        count = CountVectorizer(ngram_range=ngram_range, stop_words=stop_words, min_df=0.1, max_df=0.8).fit(docs)
+    except:
+        return 
     t = count.transform(docs).toarray()
     w = t.sum(axis=1)
     tf = np.divide(t.T, w)
@@ -81,7 +86,7 @@ def extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20):
     labels = list(docs_per_topic.topic)
     tf_idf_transposed = tf_idf.T
     indices = tf_idf_transposed.argsort()[:, -n:]
-    top_n_words = {label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
+    top_n_words = {label: [(words[j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
     return top_n_words
 
 def extract_topic_sizes(df):
@@ -94,22 +99,36 @@ def extract_topic_sizes(df):
     return topic_sizes
 
 def topic_per_link(top_n_words, docs_df):
-    url_topic_mapping = dict()
-    for ix in range(len(docs_df['canon_url'])): url_topic_mapping[docs_df['canon_url'][ix]] = top_n_words[docs_df['topic'][ix]][:5]
+    url_topic_mapping = list()
+    for ix in range(len(docs_df['canon_url'])): 
+        mapping = dict()
+        mapping['canon_url'] = docs_df['canon_url'][ix]
+        mapping['topics'] = top_n_words[docs_df['topic'][ix]][:5]
+        url_topic_mapping.append(mapping)
     return url_topic_mapping
 
 if __name__ == '__main__':
     # decompress_zstandard_to_folder('./covidstatebroadcaster.fixed.jsonl.*')
     tb, links = jsonl_reader('./covidstatebroadcaster.fixed.jsonl')
-    stop_words_all = json.loads('./stopwords-all.json')
+    with open('./stopwords-all.json', 'r') as s:
+        stop_words_all = json.load(s)
     url_topic_dict = dict()
+    fp = open('url_topic.json', 'a+')
     for lang in tb:
         em, clusters, docs_df, docs_per_topic = bert_topic(lang, tb, links)
-        tf_idf, count = c_tf_idf(docs_per_topic.doc.values, m=len(tb[lang]), stop_words_all[lang])
+        if len(tb[lang].split('\t\t')[:-1]) < 10:
+            continue
+        try:
+            tf_idf, count = c_tf_idf(docs_per_topic.doc.values, 
+                    len(tb[lang]), 
+                    stop_words=stop_words_all[lang])
+        except TypeError:
+            continue
+        except:
+            tf_idf, count = c_tf_idf(docs_per_topic.doc.values,
+                    len(tb[lang]))
         top_n_words = extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20)
-        topic_sizes = extract_topic_sizes(docs_df); topic_sizes.head(10)
+        topic_sizes = extract_topic_sizes(docs_df)
         url_topic_lang = topic_per_link(top_n_words, docs_df)
-        url_top_dict.update(url_topic_lang)
         print(url_topic_lang)
-    with open('url_topic.jsonl', 'w+') as fp:
-            json.dump(url_topic_dict, fp)
+        json.dump(url_topic_lang, fp)
