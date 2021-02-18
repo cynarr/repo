b5e8dc669baa1867eb886992f6bb53ad78c5f7f4
Frankie Robertson <frankie@robertson.name>
2021-02-18 11:52:42 +0200

    Add --debug flag for country to get list of labels

--- mmmbgknow/searchers/country.py
+++ mmmbgknow/searchers/country.py
@@ -5,7 +5,7 @@ import sys
 
 from ..utils.csv import read_csv_map_map_set
 from ..european import LANGDETECT_EURO_LANGUAGES
-from ..search import MapSearcher
+from ..search import get_re_bits, MapSearcher
 
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -13,6 +13,14 @@ COUNTRY_LABELS = (
     read_csv_map_map_set(pjoin(DIR_PATH, "..", "data/country_labels.csv"))
 )
 COUNTRY_LABELS["no"] = {**COUNTRY_LABELS["nb"], **COUNTRY_LABELS["nn"]}
+
+
+if len(sys.argv) == 3 and sys.argv[1] == "--debug":
+    lang = sys.argv[2]
+    all_country_labels = COUNTRY_LABELS[lang]
+    sys.stdout.buffer.write(b"\n".join(get_re_bits(all_country_labels)))
+    sys.stdout.buffer.write(b"\n")
+    sys.exit()
 
 
 searchers = {}
